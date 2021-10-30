92070999c807115d9bdb00281261737985e8d773
jp0617 <juan.ramirez6323@unaula.edu.co>
2021-10-30 14:56:29 -0500

    columnas extra

--- src/models/category.models.js
+++ src/models/category.models.js
@@ -5,6 +5,7 @@ const sequilize = require("../connections/connectiondb");
 const Category = sequilize.define("tblCategoria", {  
   strdesccat: { type: DataTypes.STRING },
   strnomcat: { type: DataTypes.STRING },
+  strUrl:{type:DataTypes.STRING},
   idcat: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true }  
 },{  
   timestamps:false,
--- src/models/product.models.js
+++ src/models/product.models.js
@@ -6,7 +6,8 @@ const Product = sequilize.define("tblProducto", {
   strnomprod: { type: DataTypes.STRING },
   intidcat: { type: DataTypes.INTEGER },
   intuniprod: { type: DataTypes.INTEGER },
-  idprod: { type: DataTypes.STRING, primaryKey: true}  
+  idprod: { type: DataTypes.STRING, primaryKey: true},
+  dtfecha_ingreso:{type:DataTypes.DATE}  
 },{  
   timestamps:false,
   freezeTableName:true
